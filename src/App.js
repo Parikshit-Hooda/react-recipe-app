cf6656becab2ddf2c8dae44cecf5d91a5f8fcd08
Parikshit Hooda <phooda804@live.com>
2021-12-28 02:13:39 +0530

    recipe input form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./styles.css";
 import { YOUR_APP_ID, YOUR_APP_KEY } from "./key";
 import axios from "axios";
+import { useState } from "react";
 
 export default function App() {
+  const [query, setQuery] = useState("");
+
   var url = `https://api.edamam.com/search?q=&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=alcohol-free`;
 
   async function getRecipes() {
@@ -13,7 +16,18 @@ export default function App() {
 
   return (
     <div className="app">
-      <button onClick={getRecipes}>Get recipes</button>
+      <h3>Recipe hut</h3>
+      <form className="app__searchForm">
+        <input
+          type="text"
+          placeholder="enter ingredient"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit" value="Search recipe">
+          Search recipe
+        </button>
+      </form>
     </div>
   );
 }
